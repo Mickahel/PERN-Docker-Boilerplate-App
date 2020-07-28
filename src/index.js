f8f3b80534b9ed7259cc2169f2b932d63fbd0812
Mickahel <df.michelangelo@gmail.com>
2020-07-28 09:41:22 +0200

    removed CRA service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import RoundLoader from "./components/RoundLoader";
 import "./sass/main.scss";
 ReactDOM.render(
@@ -11,13 +10,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-/*serviceWorker.register({
-  onUpdate: function () {
-    if (alert(`New content is available!. Click OK to refresh`)) {
-      window.location.reload();
-    }
-    let event = new Event("app-update");
-    window.dispatchEvent(event);
-  },
-});
-*/
